f29a5a0d866a28dab4590bba28ae538d668b78d3
Think-007 <wylipengfei@163.com>
2017-10-25 23:21:36 +0800

    hessian

--- src/main/java/com/thinker/news/ArdNewsApplication.java
+++ src/main/java/com/thinker/news/ArdNewsApplication.java
@@ -2,8 +2,10 @@ package com.thinker.news;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource({ "classpath:hessian/hessian-client.xml" })
 public class ArdNewsApplication {
 
 	public static void main(String[] args) {
